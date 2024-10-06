c6ac7fc87a67f0ff34366f1330019bdee75123e5
htymfield <htymfield@gmail.com>
2024-10-06 11:40:28 +0900

    filelist

--- TSDivider/Program.cs
+++ TSDivider/Program.cs
@@ -1,20 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.CommandLine;
+using System.Globalization;
 
 class Program {
     static async Task Main(string[] args) {
         var rootCommand = new RootCommand("TSDivider");
 
-        var dirArgument = new Argument<string>(
+        var dirArgument = new Argument<DirectoryInfo>(
             name: "dir",
-            description: "Target directory");
+            description: "Target directory.").ExistingOnly();
         rootCommand.Add(dirArgument);
 
-        rootCommand.SetHandler((dirArgValue) => {
-            Console.WriteLine("test");
-        }, dirArgument);
+        var dryRunOption = new Option<bool>(
+            name: "--dry-run",
+            description: "An option for dry run.");
+        rootCommand.Add(dryRunOption);
+
+        rootCommand.SetHandler((dirArgValue, dryRunArgValue) => {
+            Console.WriteLine("Arguments: ");
+            Console.WriteLine($"  Target directory: {dirArgValue}");
+            if (dryRunArgValue) { Console.WriteLine("  Dry run: True"); }
+
+            var solver = new Solver(
+                dirArgValue,
+                dryRunArgValue,
+                [".mp4", ".ts"]
+                );
+            solver.Invoke();
+
+        }, dirArgument, dryRunOption);
 
         await rootCommand.InvokeAsync(args);
+    }
+}
+
+
+class Solver(
+    DirectoryInfo targetDir,
+    bool isDryRun,
+    List<string> extList) {
+
+    public void Invoke() {
+        var fileList = targetDir.EnumerateFiles("*.*")
+            .Where(f => extList.Any(e => f.FullName.Contains(e)));
+
     }
 }
