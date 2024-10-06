18367cb8cbb269ea24fc062c12ac75f9efede848
htymfield <htymfield@gmail.com>
2024-10-06 10:56:16 +0900

    dir argument

--- TSDivider/Program.cs
+++ TSDivider/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.CommandLine;
+
+class Program {
+    static async Task Main(string[] args) {
+        var rootCommand = new RootCommand("TSDivider");
+
+        var dirArgument = new Argument<string>(
+            name: "dir",
+            description: "Target directory");
+        rootCommand.Add(dirArgument);
+
+        rootCommand.SetHandler((dirArgValue) => {
+            Console.WriteLine("test");
+        }, dirArgument);
+
+        await rootCommand.InvokeAsync(args);
+    }
+}
